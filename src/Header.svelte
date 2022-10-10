996ff271cdae6931c5d605d07e8a336f2069febd
OGrawis <ranggajack99@gmail.com>
2022-10-10 12:33:27 +0700

    first commit

---
+++ src/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+    import logo from '../static/favicon.png'
+  </script>
+  
+  <header class="global-header">
+    <div class="app-name">
+      <img src={logo} alt="Svelte Logo" class="logo"/>
+      <div class="title">Notes App</div>
+    </div>
+  </header>
+  
+  <style>
+    .global-header {
+      background-color: #313131;
+      height: 56px;
+      color: white;
+      padding: 15px;
+      box-sizing: border-box;
+  
+    }
+    .logo {
+        height: 100%;
+        margin-right: 5px;
+    }
+    .title {
+        font-weight: bold;
+    }
+    .app-name {
+        display: flex;
+        align-items: center;
+        height: 100%;        
+     }
+  </style>
